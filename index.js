801eee2418b7b55ff825313f533aa0ae179be86c
mcronin79 <mikecronin79@gmail.com>
2020-01-13 21:17:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -134,6 +134,8 @@ const getAndPushData = async function getAndPushData(sheetKey) {
     // TODO: Fix the weird double negative here
     const dataUnchanged = deepEqual(oldData, newData);
     if (!dataUnchanged) {
+     winston.log('info', 'start_ ' + newData);
+     winston.log('info', 'end_ ');
      winston.log('info', 'oldData: ' + oldData.length + 'newData: ' + newData.length);
      var oldDataEntries = Object.keys(oldData).length;
      var newDataEntries = Object.keys(newData).length;
